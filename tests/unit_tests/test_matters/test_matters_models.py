8a001d1aeb889765343e1b06f218bda9303c9e7e
Anna Makarudze <amakarudze@gmail.com>
2022-05-24 18:29:14 +0200

    Add tests and migrations for matters models

--- tests/unit_tests/test_matters/test_matters_models.py
+++ tests/unit_tests/test_matters/test_matters_models.py
@@ -1,2 +1,9 @@
-def test_setup():
-    pass
+def test_conveyance_matter_model_string_representation(conveyance_matter):
+    assert (
+        str(conveyance_matter)
+        == "Deeds Transfer between Jane Doe and First Capital Bank"
+    )
+
+
+def test_bank_model_string_representation(bank):
+    assert str(bank) == "First Capital Bank"
